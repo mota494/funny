cb6987c683a6f8f5c691c44787ef346cbfbb35af
Miguel Mota <migueloureiromota@gmail.com>
2024-10-07 16:02:51 +0100

    Serious business
    Serious business

--- change.sh
+++ change.sh
@@ -1,4 +1,4 @@
 rm image.png
 curl -o image.png 'https://static.wikia.nocookie.net/monstermovies/images/0/01/Whalemingo.jpg/revision/latest?cb=20160619230613'
-gsettings set org.gnome.desktop.background picture-uri-dark ~/image.png
-gsettings set org.gnome.desktop.background picture-uri ~/image.png
+gsettings set org.gnome.desktop.background picture-uri-dark ~/funny/image.png
+gsettings set org.gnome.desktop.background picture-uri ~/funny/image.png
