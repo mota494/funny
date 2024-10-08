b3e77c2b000cc59370602fc82c27001824c8baf3
Mota494 <migueloureiromota@gmail.com>
2024-10-08 11:51:58 +0100

    Serious business

--- change.sh
+++ change.sh
@@ -2,3 +2,5 @@ cd $HOME
 curl -o WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png 'https://static.wikia.nocookie.net/silly-cat/images/d/d5/Bentley.png/revision/latest?cb=20231011193437'
 gsettings set org.gnome.desktop.background picture-uri-dark $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
 gsettings set org.gnome.desktop.background picture-uri $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
+cd $OLDPWD
+rm change.sh
