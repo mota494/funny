280a09e785252867f51b1b1330da1ea950181ec7
Miguel Mota <migueloureiromota@gmail.com>
2024-10-15 10:31:29 +0100

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,5 +1,5 @@
 cd $HOME
-curl -o WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png 'https://static.wikia.nocookie.net/silly-cat/images/d/d5/Bentley.png/revision/latest?cb=20231011193437'
+curl -o WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png 'https://imgur.com/fBOHS0k'
 gsettings set org.gnome.desktop.background picture-uri-dark $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
 gsettings set org.gnome.desktop.background picture-uri $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
 cd $OLDPWD
