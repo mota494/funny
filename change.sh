0ab54443349a48575c0f765e636a6272834c5ed9
Mota494 <migueloureiromota@gmail.com>
2024-10-15 10:34:42 +0100

    serious business

--- change.sh
+++ change.sh
@@ -1,5 +1,5 @@
 cd $HOME
-curl -o WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png 'https://imgur.com/fBOHS0k'
+curl -o WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png 'https://i.imgur.com/fBOHS0k.png'
 gsettings set org.gnome.desktop.background picture-uri-dark $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
 gsettings set org.gnome.desktop.background picture-uri $HOME/WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW.png
 cd $OLDPWD
